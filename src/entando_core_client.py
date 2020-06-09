99270f149b8ff9ca168b89a10af8d344a3cdd472
Werbth Rocha <werbth@gmail.com>
2020-06-08 22:28:40 -0300

    Process page models

--- src/entando_core_client.py
+++ src/entando_core_client.py
@@ -12,23 +12,33 @@ class EntandoClient:
         self._headers = {'authorization': 'Bearer ' + access_token}
 
     def update_widget(self, widget):
-        logging.info("Loading Widget {}".format(widget["code"]))
         r = requests.put(self._url + "/api/widgets/" + widget["code"],
                          json=widget, headers=self._headers)
         if r.status_code == 404:
-            requests.post(self._url + "/api/widgets", json=widget,
-                          headers=self._headers)
+            r = requests.post(self._url + "/api/widgets", json=widget,
+                              headers=self._headers)
+        logging.info("Loading Widget {}: {}".format(widget["code"],
+                                                    r.status_code))
 
     def update_fragment(self, fragment):
-        logging.info("Loading fragment {}".format(fragment["code"]))
         r = requests.put(self._url + "/api/fragments/" + fragment["code"],
                          json=fragment, headers=self._headers)
         if r.status_code == 404:
-            requests.post(self._url + "/api/fragments", json=fragment,
-                          headers=self._headers)
+            r = requests.post(self._url + "/api/fragments", json=fragment,
+                              headers=self._headers)
+        logging.info("Loading fragment {}: {}".format(fragment["code"],
+                                                      r.status_code))
+
+    def update_page_model(self, page_model):
+        r = requests.put(self._url + "/api/pageModels/" + page_model["code"],
+                         json=page_model, headers=self._headers)
+        if r.status_code == 404:
+            r = requests.post(self._url + "/api/pageModels", json=page_model,
+                              headers=self._headers)
+        logging.info("Loading page model {}: {}".format(page_model["code"],
+                                                        r.status_code))
 
     def upload_resource(self, resource):
-        logging.info("Uploading resource {}".format(resource["path"]))
         payload = {'protectedFolder': False, 'currentPath': resource['path']}
         r = requests.get(self._url + "/api/fileBrowser/file", params=payload,
                          headers=self._headers)
@@ -39,8 +49,8 @@ class EntandoClient:
         else:
             r = requests.put(self._url + "/api/fileBrowser/file", json=resource,
                              headers=self._headers)
-        logging.info(
-            "Result for {}: {}".format(resource["path"], r.status_code))
+        logging.info("Uploading resource {}: {}".format(resource["path"],
+                                                        r.status_code))
 
     def create_directory(self, directory_name):
         path = Path(directory_name)
@@ -51,9 +61,8 @@ class EntandoClient:
                 'protectedFolder': False,
                 'path': '/' + current_name
             }
-            logging.info(
-                "Creating directory {} on Entando".format('/' + current_name))
             r = requests.post(self._url + "/api/fileBrowser/directory",
                               json=directory, headers=self._headers)
-            logging.info("Result for directory {}: {}".format(current_name,
-                                                              r.status_code))
+            logging.info(
+                "Creating directory {}: {}".format('/' + current_name,
+                                                   r.status_code))
--- src/loader.py
+++ src/loader.py
@@ -29,6 +29,10 @@ class Loader:
             fragments = descriptor_yaml['components']['fragments']
             self._load_fragments(bundle_directory, fragments)
 
+            # load page models
+            page_models = descriptor_yaml['components']['pageModels']
+            self._load_page_models(bundle_directory, page_models)
+
     def _upload_resources(self, bundle_directory, descriptor_code):
         logging.info("Uploading resources -------")
         path = bundle_directory / "resources"
@@ -65,8 +69,22 @@ class Loader:
         for fragment in fragments:
             path = bundle_directory / fragment
             with path.open('r') as f:
-               fragment_yaml = yaml.load(f, Loader=yaml.FullLoader)
-               fragment_yaml['guiCode'] = (
-                       path.parent / fragment_yaml['guiCodePath']).read_text()
-               self._client.update_fragment(fragment_yaml)
+                fragment_yaml = yaml.load(f, Loader=yaml.FullLoader)
+                fragment_yaml['guiCode'] = (
+                        path.parent / fragment_yaml['guiCodePath']).read_text()
+                self._client.update_fragment(fragment_yaml)
 
+    def _load_page_models(self, bundle_directory, page_models):
+        logging.info("Loading page models -------")
+        for page_model in page_models:
+            path = bundle_directory / page_model
+            with path.open('r') as f:
+                pm_yaml = yaml.load(f, Loader=yaml.FullLoader)
+                pm_yaml['template'] = (
+                        path.parent / pm_yaml['templatePath']).read_text()
+                pm_yaml['descr'] = pm_yaml['description']
+                if 'configuration' in pm_yaml and 'frames' in pm_yaml[
+                    'configuration']:
+                    for frame in pm_yaml['configuration']['frames']:
+                        frame['descr'] = frame['description']
+                self._client.update_page_model(pm_yaml)
